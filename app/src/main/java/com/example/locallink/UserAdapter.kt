34dab549369a8836e4cbaf4b2c6c93fabefb9978
Danny Son <son.d@northeastern.edu>
2023-12-14 01:40:35 -0500

    creating user pop up, and added more mock data of students

--- app/src/main/java/com/example/locallink/UserAdapter.kt
+++ app/src/main/java/com/example/locallink/UserAdapter.kt
@@ -1,12 +1,14 @@
 package com.example.locallink
 
 import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.SharedPreferences
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
@@ -52,18 +54,46 @@ class UserAdapter(private val list: MutableList<User>,
         holder.majorText.text = list[position].major
         holder.name.text = list[position].name
         holder.viewMoreButton.setOnClickListener {
-           // displayPopUp(holder, position)
-            Log.i("adapter", "clicking on view more with name: ${list[position].name}")
+           displayPopUp(position)
+        }
+        holder.chatButton.setOnClickListener {
+            // move to chat with user
         }
 
     }
 
-    private fun displayPopUp(holder: UserViewHolder, position: Int) {
+    private fun displayPopUp(position: Int) {
         val builder = AlertDialog.Builder(fragment.context)
-        builder.apply {
-            val customLayout: View = fragment.layoutInflater.inflate(R.layout.building_popup, null)
-            setView(customLayout)
+        val dialog = builder.create()
+        val view: View = fragment.layoutInflater.inflate(R.layout.user_pop_up, null)
+        val nameText: TextView = view.findViewById(R.id.user_popup_name)
+        val cancelIcon: ImageView = view.findViewById(R.id.user_popup_cancel_icon)
+        val pronounText: TextView = view.findViewById(R.id.user_popup_pronoun_body)
+        val locationText: TextView = view.findViewById(R.id.user_popup_location_body)
+        val majorText: TextView = view.findViewById(R.id.user_popup_major_body)
+        val classesText: TextView = view.findViewById(R.id.user_popup_classes_body)
+        val interestsText: TextView = view.findViewById(R.id.user_popup_interests_body)
+        val bioText: TextView = view.findViewById(R.id.user_popup_bio_body)
+
+        val chatButton: Button = view.findViewById(R.id.user_popup_chat)
+        chatButton.setOnClickListener {
+            //nav to chat screen
         }
-        builder.show()
+
+        nameText.text = list[position].name
+        pronounText.text = list[position].pronouns
+        locationText.text = list[position].building
+        majorText.text = list[position].major
+        classesText.text = list[position].classes
+        interestsText.text = list[position].interests
+        bioText.text = list[position].bio
+
+        cancelIcon.setOnClickListener {
+            dialog.dismiss()
+        }
+
+
+        dialog.setView(view)
+        dialog.show()
     }
 }
